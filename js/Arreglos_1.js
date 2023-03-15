0d23001233f0e0c15e33201c1c9590070b16bae1
emanuel ruiz <ruizemanuelok@gmail.com>
2023-03-15 20:15:24 -0300

    arreglos 1 y 2

---
+++ js/Arreglos_1.js
@@ -0,0 +1,7 @@
+let meses = ["enero", "febrero","marzo","abril","mayo","junio","julio", "agosto","sptiembre","octubre", "noviembre","diciembre"]
+for (i=0 ; i<=11; i++) {
+    document.write("<ul>"+
+        "<li>"+ meses[i]+ 
+        "</li>"+
+    "</ul>")
+}
